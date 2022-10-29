a407659db64f556d1acb6d118483b0f18bd6a68d
Alexander EG#9070 <116421926+AlexEG@users.noreply.github.com>
2022-10-29 17:36:07 +0200

    make my todo visible on the page

--- js/app.js
+++ js/app.js
@@ -1,15 +1,22 @@
 let myTasks = ['task 1', 'task 2', 'task 3'];
 
-myTasks.forEach(function (todoName) {
-  let elem = document.createElement('div');
-  elem.innerText = todoName;
-  document.body.appendChild(elem);
-});
-
 // make a add todo button
+letMeSeeMyTodo();
 
 function addTodo() {
   let textbox = document.getElementById('typeYourTodo');
   let title = textbox.value;
   myTasks.push(title);
+  letMeSeeMyTodo();
+}
+
+// so I can't see the new todo on the page
+// so to fix that ...
+
+function letMeSeeMyTodo() {
+  myTasks.forEach(function (todoName) {
+    let elem = document.createElement('div');
+    elem.innerText = todoName;
+    document.body.appendChild(elem);
+  });
 }
